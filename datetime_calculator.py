0e380175ae123fe432484d8ccee956f1f4321b6c
Luan Nguyen <duyluandethuong@gmail.com>
2024-07-04 06:32:28 +0700

    Update README

--- datetime_calculator.py
+++ datetime_calculator.py
@@ -4,10 +4,10 @@ import sys
 print(sys.argv)
 
 def convert_seconds_to_hhmmss(seconds):
-    hours = seconds // 3600
-    minutes = (seconds % 3600) // 60
-    seconds = seconds % 60
-    return f"{hours:02}:{minutes:02}:{seconds:02}"
+    hours = int(seconds // 3600)
+    minutes = int((seconds % 3600) // 60)
+    seconds = int(seconds % 60)
+    return f"{hours:02}:{minutes:01}:{seconds:02}"
 
 # start time
 start_time = sys.argv[1]
